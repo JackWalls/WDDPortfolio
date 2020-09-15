51bd71e23bee1e484288bbadbb048f8bd684ef92
JackWalls <44418813+JackWalls@users.noreply.github.com>
2020-09-15 14:55:48 -0700

    Fixed main.js Table of Contents ready.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-/*function createList(){
+function createList(){
     var links = [
         {label: "Week 1", url: "week1/index.html"},
         {label: "Week 2", url: "week2/index.html"}
     ];
     var text = "";
     for (var i = 0; i < 2; i++){
-        text += "<li><a href='" + links[i].url + "'>"+ links[i].label + "<a/><li/>";
+            text += "<li><a href='" + links[i].url + "'>" + links[i].label;
     }
     document.getElementById("list").innerHTML = text;
-}*/
+}
 
 
 
