37da8e70e031c9bb8103761c575c241e4c0ea750
JackWalls <44418813+JackWalls@users.noreply.github.com>
2020-09-15 14:25:19 -0700

    Table of contents page created, main.js problem with for loop creating extra li in between items.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*function createList(){
+    var links = [
+        {label: "Week 1", url: "week1/index.html"},
+        {label: "Week 2", url: "week2/index.html"}
+    ];
+    var text = "";
+    for (var i = 0; i < 2; i++){
+        text += "<li><a href='" + links[i].url + "'>"+ links[i].label + "<a/><li/>";
+    }
+    document.getElementById("list").innerHTML = text;
+}*/
+
+
+
