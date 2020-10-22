016671758e8b539a3af37dce5c7b79b50c1303f7
JackWalls <44418813+JackWalls@users.noreply.github.com>
2020-10-22 15:54:46 -0700

    Enabled sorting, all that is left to do is separate the js into modules and finishing touches to css. Maybe add extra features.

--- js/ToDos.js
+++ js/ToDos.js
@@ -1,20 +1,63 @@
-/*const tasks = [
-    {id: 1, content: "Task 1", completed: true},
-    {id: 2, content: "Task 2", completed: false},
-    {id: 3, content: "Task 3", completed: false},
-];*/
-//localStorage.setItem('list',JSON.stringify(tasks));
-
+// import
+/*********************************************************************
+ * This event handler will be used to initialize all other event
+ * listeners for the web application using functions from other
+ * modules, a nested function is used to keep an array of the buttons
+ * for each task. It has to be nested since it will get the array of
+ * buttons after displayList is called.
+ * A fault I found with using a display function to update the list
+ * every time a change was made is that new event listeners need
+ * to be attached to the new button elements.
+ */
 window.onload = () => {
+    // This function solves the issue mentioned in the function header.
+    function resetListeners() {
+        // If the list is empty then do nothing.
+        if(buttons.length > 0) {
+            // For older browsers it is important to remove all lost event listeners to avoid a memory leak.
+            buttons.forEach(button => {
+                button.removeEventListener('click', removeTask);
+            });
+
+            // Update array of button elements.
+            buttons = document.querySelectorAll('.remove');
+
+            // These two lines add event listeners to each new button. A call to resetListeners
+            // is needed to maintain all the event listeners on the buttons.
+            buttons.forEach(button => {
+                button.addEventListener('click', removeTask);
+            });
+            buttons.forEach(button => {
+                button.addEventListener('click', resetListeners);
+            });
+        }
+    }
+
     displayList(toDos);
-    const checkbox = document.querySelector('#toDo');
-    checkbox.addEventListener('change', changeStatus);
-    const button = document.querySelector("button");
-    button.addEventListener('click', removeTask);
+    // Create the array after the list is displayed.
+    let buttons = document.querySelectorAll('.remove');
+
+    // If the list is empty then do nothing.
+    if(buttons.length > 0) {
+        buttons.forEach(button => {
+            button.addEventListener('click', removeTask);
+        });
+        buttons.forEach(button => {
+            button.addEventListener('click', resetListeners);
+        });
+    }
+
+    // Listeners to handle adding tasks to the list.
+    document.addEventListener('submit', appendList);
+    document.addEventListener('submit', resetListeners);
+
+    // Listeners to handle sorting the list.
+    document.querySelector('#sort').addEventListener('change', sortList);
+    document.querySelector('#sort').addEventListener('change', resetListeners);
+
+    // Listener for changing the completed status of a task.
+    document.querySelector('#toDo').addEventListener('change', changeStatus);
 };
-
-document.addEventListener('submit', appendList);
-
 
 /****************************************************/
 let toDos = getList();
@@ -23,47 +66,32 @@ function getList() {
     return JSON.parse(localStorage.getItem('list'));
 }
 
+function updateLocalStorage() {
+    localStorage.setItem('list',JSON.stringify(toDos));
+}
+
 function displayList(toDo) {
     let elements = "";
     for (let i = 0; i < toDo.length; i++){
         if (toDo[i].completed)
             elements += `<li id="${toDo[i].id}" style="text-decoration: line-through;"><input type="checkbox" name="tasks" checked/>
-                 ${toDo[i].content} <button>X</button></li>`;
+                 ${toDo[i].content} <button class="remove">X</button></li>`;
         else
             elements += `<li id="${toDo[i].id}"><input type="checkbox" name="tasks"/> 
-                    ${toDo[i].content} <button>X</button></li>`;
+                    ${toDo[i].content} <button class="remove">X</button></li>`;
     }
     document.querySelector("#list").innerHTML = elements;
     //document.getElementById('list').innerHTML = list;
 }
 
 function appendList(event) {
-    const input = document.forms[1].addTo.value;
+    event.preventDefault();
+    const input = document.forms[0].addTo.value;
     const add = {id: Date.now(), content: input, completed: false};
     toDos.unshift(add);
     updateLocalStorage();
     displayList(toDos);
-    event.preventDefault();
-}
 
-function changeStatus(event) {
-    let index = findIndexById(event.target.parentElement.id);
-    if (event.target.checked) {
-        event.target.parentElement.style.textDecoration = "line-through";
-        if (index !== false) {
-            toDos[index].completed = true;
-            updateLocalStorage();
-        }
-
-    }
-    else {
-        event.target.parentElement.style.textDecoration = "none";
-        toDos[findIndexById(event.target.parentElement.id)].completed = false;
-        if (index !== false) {
-            toDos[index].completed = false;
-            updateLocalStorage();
-        }
-    }
 }
 
 function removeTask(event){
@@ -75,9 +103,46 @@ function removeTask(event){
     }
 }
 
-function updateLocalStorage() {
-    localStorage.setItem('list',JSON.stringify(toDos));
+function sortList(event) {
+    switch(parseInt(event.target.value)) {
+        case 0:
+            toDos.sort((a, b) => {return b.id - a.id;});
+            displayList(toDos);
+            break;
+        case 1:
+            toDos.sort((a, b) => {
+                if(a.completed)
+                    return 1;
+                return b.id - a.id;
+            });
+            displayList(toDos);
+            break;
+        case 2:
+            toDos.sort((a, b) => {
+                if(!a.completed)
+                    return 1;
+                return b.id - a.id;
+            });
+            displayList(toDos);
+            break;
+    }
 }
+function changeStatus(event) {
+    let index = findIndexById(event.target.parentElement.id);
+    if (index !== false) {
+        if (event.target.checked) {
+            event.target.parentElement.style.textDecoration = "line-through";
+            toDos[index].completed = true;
+            updateLocalStorage();
+        }
+        else {
+            event.target.parentElement.style.textDecoration = "none";
+            toDos[index].completed = false;
+            updateLocalStorage();
+        }
+    }
+}
+
 function findIndexById(check){
     for(let i = 0; i < toDos.length; i ++) {
         if (toDos[i].id === parseInt(check)) {
