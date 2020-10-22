074e5c995b537e43c072e538d08dd838a3323521
JackWalls <44418813+JackWalls@users.noreply.github.com>
2020-10-21 17:15:06 -0700

    Enabled complete updating and removal of tasks.

--- js/ToDos.js
+++ js/ToDos.js
@@ -4,15 +4,31 @@
     {id: 3, content: "Task 3", completed: false},
 ];*/
 //localStorage.setItem('list',JSON.stringify(tasks));
+
+window.onload = () => {
+    displayList(toDos);
+    const checkbox = document.querySelector('#toDo');
+    checkbox.addEventListener('change', changeStatus);
+    const button = document.querySelector("button");
+    button.addEventListener('click', removeTask);
+};
+
+document.addEventListener('submit', appendList);
+
+
+/****************************************************/
 let toDos = getList();
 
+function getList() {
+    return JSON.parse(localStorage.getItem('list'));
+}
 
 function displayList(toDo) {
     let elements = "";
     for (let i = 0; i < toDo.length; i++){
         if (toDo[i].completed)
-            elements += `<li id="${toDo[i].id}"><input type="checkbox" name="tasks" checked/>
-                <del> ${toDo[i].content} </del><button>X</button></li>`;
+            elements += `<li id="${toDo[i].id}" style="text-decoration: line-through;"><input type="checkbox" name="tasks" checked/>
+                 ${toDo[i].content} <button>X</button></li>`;
         else
             elements += `<li id="${toDo[i].id}"><input type="checkbox" name="tasks"/> 
                     ${toDo[i].content} <button>X</button></li>`;
@@ -21,24 +37,52 @@ function displayList(toDo) {
     //document.getElementById('list').innerHTML = list;
 }
 
-if(getList() !== null)
-    window.onload = () => displayList(toDos);
-
-document.addEventListener('submit', appendList);
-/****************************************************/
-
-function getList() {
-    return JSON.parse(localStorage.getItem('list'));
-}
-
 function appendList(event) {
-    const input = document.forms[0].addTo.value;
+    const input = document.forms[1].addTo.value;
     const add = {id: Date.now(), content: input, completed: false};
     toDos.unshift(add);
-    localStorage.setItem('list',JSON.stringify(toDos));
+    updateLocalStorage();
     displayList(toDos);
     event.preventDefault();
 }
 
+function changeStatus(event) {
+    let index = findIndexById(event.target.parentElement.id);
+    if (event.target.checked) {
+        event.target.parentElement.style.textDecoration = "line-through";
+        if (index !== false) {
+            toDos[index].completed = true;
+            updateLocalStorage();
+        }
 
-//function findIndexById(){};
+    }
+    else {
+        event.target.parentElement.style.textDecoration = "none";
+        toDos[findIndexById(event.target.parentElement.id)].completed = false;
+        if (index !== false) {
+            toDos[index].completed = false;
+            updateLocalStorage();
+        }
+    }
+}
+
+function removeTask(event){
+    let index = findIndexById(event.target.parentElement.id);
+    if(confirm(`Do you want to remove this task? "${toDos[index].content}"`)){
+        toDos.splice(index, 1);
+        updateLocalStorage();
+        displayList(toDos);
+    }
+}
+
+function updateLocalStorage() {
+    localStorage.setItem('list',JSON.stringify(toDos));
+}
+function findIndexById(check){
+    for(let i = 0; i < toDos.length; i ++) {
+        if (toDos[i].id === parseInt(check)) {
+            return i;
+        }
+    }
+    return false;
+}
